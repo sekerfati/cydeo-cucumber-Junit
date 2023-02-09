dc837b31f2ce0a315287077e5540db4a3b246b00
Fatih Seker <fatihzucker@gmail.com>
2023-02-09 19:35:34 +0100

    some changes

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue="com/cydeo/step_definitions",
         dryRun = false,
