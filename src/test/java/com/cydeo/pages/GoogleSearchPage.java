03018a782d32fd8d566fa56fe74c937a9993e79b
Fatih Seker <fatihzucker@gmail.com>
2023-02-20 18:47:14 +0100

    some changes

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,13 +1,22 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
 
    public GoogleSearchPage(){
+  //Create constructor
+  // initialize the driver instance and this class' instance using     PageFactory.initElements
        PageFactory.initElements(Driver.getDriver(), this);
    }
+
+// we can start locate web elements using @FindBy annotation
+ @FindBy(name = "q")
+ public WebElement searchBox;
+
 
 
 
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,19 +1,32 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
+    GoogleSearchPage googleSearchPage=new GoogleSearchPage();
+
     @When("user enter apple and clicks enter")
     public void user_enter_apple_and_clicks_enter() {
-
+googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
     }
+
+
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
+String expectedTitle="apple - Sök på Google";
+String actualTitle=Driver.getDriver().getTitle();
+//Junit assertion accepts first argument as expected and second argument as actual
+Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+//Assert.assertTrue(expectedTitle.equals(actualTitle));
+
+
 
     }
 
