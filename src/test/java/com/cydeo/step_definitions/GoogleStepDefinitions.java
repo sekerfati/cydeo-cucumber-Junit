ef950678351bf4fb6df7b132df6c225251d722c7
Fatih Seker <fatihzucker@gmail.com>
2023-02-21 11:01:30 +0100

    some changes

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -12,25 +12,38 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage=new GoogleSearchPage();
 
-    @When("user enter {string} and clicks enter")
-    public void user_enter_and_clicks_enter(String string) {
+    @When("user enter {word} and clicks enter")
+    public void user_enter_and_clicks_enter2(String searchKeyword) {
+        googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
 
     }
+
+
+
+    @When("user enter {string} and clicks enter")
+    public void user_enter_and_clicks_enter(String searchKeyword) {
+googleSearchPage.searchBox.sendKeys(searchKeyword+Keys.ENTER);
+
+    }
+
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
+        String expectedTitle=string+" - Sök på Google";
+        String actualTitle=Driver.getDriver().getTitle();
+//Junit assertion accepts first argument as expected and second argument as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+//Assert.assertTrue(expectedTitle.equals(actualTitle));
+
 
     }
 
 
-    @When("user enter apple and clicks enter")
-    public void user_enter_apple_and_clicks_enter() {
-googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
-    }
 
 
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-String expectedTitle="apple - Sök på Google";
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String word) {
+String expectedTitle=word+" - Sök på Google";
 String actualTitle=Driver.getDriver().getTitle();
 //Junit assertion accepts first argument as expected and second argument as actual
 Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
